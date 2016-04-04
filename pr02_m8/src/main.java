cf9f1e01acc4b83145053cee31c774bcd796678d
Cristian <Cristian@Cristian-Laptop>
2016-04-04 17:20:31 +0200

    Versió 0.2  del projecte pr02_m8

--- pr02_m8/src/main.java
+++ pr02_m8/src/main.java
@@ -13,15 +13,16 @@ import java.net.UnknownHostException;
  */
 public class main {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hola món");
+       public static void main(String[] args) {
+
+// TODO code application logic here 
+        System.out.println("Hola món modificant per Cristian");
         System.out.println("versió 0.2 del projecte prjava00");
         try {
             InetAddress addr = InetAddress.getLocalHost();
             String ipAddr = addr.getHostAddress();
             String hostname = addr.getHostName();
-            System.out.println("1hostname=" + hostname);
+            System.out.println("hostname=" + hostname);
             System.out.println("Adreça IP: " + ipAddr);
             System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
             System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
@@ -29,6 +30,9 @@ public class main {
             System.out.println("Versió OS: " + System.getProperty("os.version"));
         } catch (UnknownHostException e) {
             e.printStackTrace();
+
         }
+
     }
+
 }
