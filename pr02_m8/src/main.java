1514e84bff0df9518ac8b25f1efe89d401db0402
uritm <uritm@10.10.43.146>
2016-04-04 16:55:11 +0200

    versio 2.0 projecte 2

---
+++ pr02_m8/src/main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author uritm
+ */
+public class main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola món");
+        System.out.println("Versió 0.1 del projecte pr02");
+
+    }
+}
